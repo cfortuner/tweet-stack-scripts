3ea85115ce45cc043fe61d75b02bb15b8dd7f2a6
Colin Fortuner <cfortuner@gmail.com>
2022-08-21 14:39:52 -0400

    finished open ai classifications

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import { downloadTransformedTweets } from "./scripts/download-transformed-tweets
 import fetchTwitterData from "./scripts/fetch-twitter-data.js";
 import { runTweetETL, runUsersETL } from "./scripts/twitter-ETL.js";
 import { findEduContent } from "./scripts/filter-tweets-for-edu-content.js";
-import { generateFinetuningTopicsDataset } from "./topic-classification/topic-classification.js";
+import {
+  generateFinetuningTopicsDataset,
+  getTopicPhrasesFromTweets,
+  runFineTunedTopicClassification,
+  runTopicClassification,
+} from "./topic-classification/topic-classification.js";
 import { readFile, writeFile } from "./helpers.js";
 
 // --------------------
@@ -28,4 +33,21 @@ import { readFile, writeFile } from "./helpers.js";
 // This is just a quick way to get a bunch of prompts and reasonable results.
 // results are cached unless you override with true.
 // if you fetch new data, you'll have to inspect / update each completion in the resulting file
-await generateFinetuningTopicsDataset("scratch/eduContent", "all", 100, true);
+// await generateFinetuningTopicsDataset("scratch/eduContent", "all", 100, true);
+
+// testing classification
+let res;
+//res = await runTopicClassification(
+//   "Why I think NFTs will force Hollywood, Harvard, Salesforce, Nintendo etc to reinvent themselves \n\nA thread about how NFTs will shake things up:"
+// );
+// console.log(res);
+// res = await runTopicClassification(
+//   "There's never been a harder time to hire than right now.\n\nThese 5 threads show you the step-by-step process I used to recruit A+ players when no one else could.\n\nThe Hiring Thread of All Threads 🧵👇"
+// );
+// console.log(res);
+// res = await runTopicClassification(
+//   "Why start a startup? When something is so broken and you know you can fix it. Film schools cost $250K, admit 50, and rely on “who you know” for jobs. We changed that: teaching millions of learners, paying $10M+ to workers we hire. After 5 pivots here’s how @creatorup did it. A 🧵"
+// );
+// console.log(res);
+
+// await getTopicPhrasesFromTweets("./scratch/eduContent", "all");
--- src/topic-classification/topic-classification.js
+++ src/topic-classification/topic-classification.js
@@ -1,34 +1,59 @@
 import { Configuration, OpenAIApi } from "openai";
-import fs from "fs";
+import fs, { write } from "fs";
 import config from "../config.js";
-import { readFile, shuffle, writeFile } from "../helpers.js";
+import { chunkArray, readFile, shuffle, writeFile } from "../helpers.js";
+import { sleepSecs } from "twitter-api-v2/dist/v1/media-helpers.v1.js";
 
 const configuration = new Configuration({
   apiKey: config.openai.apiKey,
 });
 const openai = new OpenAIApi(configuration);
 
-const runTopicClassification = async (doc) => {
+export const runTopicClassification = async (doc) => {
+  let completion;
+  try {
+    completion = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: generateClassificationPrompt(doc),
+      temperature: 0.6,
+      max_tokens: 50,
+    });
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+
+  return completion.data.choices;
+};
+
+export const runFineTunedTopicClassification = async (
+  doc,
+  model = "davinci:ft-personal-2022-08-21-13-54-08"
+) => {
   const completion = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: generateClassificationPrompt(doc),
-    temperature: 0.6,
-    max_tokens: 500,
+    model,
+    prompt: doc,
+    temperature: 0.2,
+    max_tokens: 30,
   });
 
   return completion.data.choices;
 };
 
 const generateClassificationPrompt = (doc) => {
-  return `Identify 1-3 topics for this text.
+  return `What are 1-3 topics for the following tweet:
 
-Text: Already feeling the weight of the week? Here's what I do when I'm already feeling overwhelmed by the week ahead. 2 steps you can do right now to banish the Sunday Scaries, be fully present, and enjoy your day. #ship30for30 #selfcare #worklifebalance
-Topics: Improving you mental health, sunday scaries, work life balance
-Text: Measuring user retention has been called as the single most important metric for Product Market Fit. But how do you start measuring it? Where do you start? Here's a 5 min primer.
-Topics: measuring user retention, product management, product market fit metrics 
-Text: I've spent 20+ years working for top companies in the world of finance. Along the way, I've made nearly every mistake in the book. nSome cost me thousands and maybe even millions of dollars. If I could speak to my 22-year-old self, here's the advice I'd give.
-Topics: financial career advice, advice to younger self, how to avoid finance mistakes
-Text: ${doc}
+Tweet: Already feeling the weight of the week? Here's what I do when I'm already feeling overwhelmed by the week ahead. 2 steps you can do right now to banish the Sunday Scaries, be fully present, and enjoy your day. #ship30for30 #selfcare #worklifebalance
+Topics: - Improving you mental health\n- work life balance\n- self care
+Tweet: Measuring user retention has been called as the single most important metric for Product Market Fit. But how do you start measuring it? Where do you start? Here's a 5 min primer.
+Topics: - measuring user retention\n- product management\n- product market fit metrics 
+Tweet: I've spent 20+ years working for top companies in the world of finance. Along the way, I've made nearly every mistake in the book. nSome cost me thousands and maybe even millions of dollars. If I could speak to my 22-year-old self, here's the advice I'd give.
+Topics: - financial career advice\n- advice to younger self\n- how to avoid finance mistakes
+Tweet: We live in the age of binge-reading. People listen to podcasts, articles, and audiobooks at 3x speed. But this speed consumption strategy is based on a flawed model of learning. Turns out, there's a more effective way to learn. 🧵",
+Topics: - learning\n- how to learn from podcasts articles and audiobooks\n- speed reading\n- productivity
+Tweet: Cognitive biases that make people buy, marketing trends, and predictions to watch out, one-person 8 figure growth marketing lessons. Here are the 9 best marketing tweets &amp; threads you might have missed last week:",
+Topics: - cognitive bias\n- marketing\n- entrepreneurship\n- growth marketing lessons
+Tweet: ${doc}
 Topics:
 
 `;
@@ -82,3 +107,43 @@ export const generateFinetuningTopicsDataset = async (
 
 // After you’ve fine-tuned a model, remember that your prompt has to end with the indicator string `\n\n###\n\n` for the model to start generating completions, rather than continuing with the prompt. Make sure to include `stop=[" undefined"]` so that the generated texts ends at the expected place.
 // Once your model starts training, it'll approximately take 4.73 minutes to train a `curie` model, and less for `ada` and `babbage`. Queue will approximately take half an hour per job ahead of you.
+
+export const getTopicPhrasesFromTweets = async (fromFolder, fromFile) => {
+  console.log(config.openai);
+  let tweets = readFile(fromFolder, fromFile);
+  const alreadyClassified = readFile(
+    "./scratch/topic-classification",
+    "./tweetsWithTopicPhrases.json"
+  );
+
+  tweets = tweets.filter((t) => {
+    return !alreadyClassified.find((ac) => {
+      return ac.tweet.tweetId === t.tweetId;
+    });
+  });
+
+  let results = [];
+
+  const processTweet = async (tweet) => {
+    const topics = await runTopicClassification(tweet.text);
+    results.push({
+      tweet,
+      topics,
+    });
+  };
+
+  for (let tweet of tweets) {
+    try {
+      await processTweet(tweet);
+    } catch (e) {
+      console.error("error processing tweet", e);
+    }
+    await sleepSecs(1);
+  }
+
+  writeFile(
+    "./scratch/topic-classification",
+    "tweetsWithTopicPhrases2.json",
+    results
+  );
+};
